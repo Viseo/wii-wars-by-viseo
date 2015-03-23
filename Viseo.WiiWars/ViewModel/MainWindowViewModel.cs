ae2b1e2fbb555b2fdce1b2bfead5ab59d89a2765
Vincent Thavo Live Viz <vincent.thavonekham@viseo.com>
2015-03-23 21:36:00 +0100

    Take into account developers with NO wiimote, but now able to debug setp-by-step : replacing one of the try-catch into a "if-statement".

--- Viseo.WiiWars/ViewModel/MainWindowViewModel.cs
+++ Viseo.WiiWars/ViewModel/MainWindowViewModel.cs
@@ -14,260 +14,271 @@ using WiimoteLib;
 
 namespace Viseo.WiiWars.ViewModel
 {
-	public sealed class MainWindowViewModel : ViewModelBase, IDisposable
-	{
-		private FilterInfoCollection _videoDevices;
-		private VideoCaptureDevice _videoSource;
-		private readonly SynchronizationContext _synchronizationContext;
-		private List<FilterInfo> _devices;
-		private FilterInfo _selectedDevice;
-		private BitmapImage _currentImage;
-		private BitmapImage _videoButtonImage;
+    public sealed class MainWindowViewModel : ViewModelBase, IDisposable
+    {
+        private FilterInfoCollection _videoDevices;
+        private VideoCaptureDevice _videoSource;
+        private readonly SynchronizationContext _synchronizationContext;
+        private List<FilterInfo> _devices;
+        private FilterInfo _selectedDevice;
+        private BitmapImage _currentImage;
+        private BitmapImage _videoButtonImage;
         private ICommand _startStopSendingToAzureCommand;
-		private ICommand _startStopVideoFeedCommand;
+        private ICommand _startStopVideoFeedCommand;
 
-		private ObservableCollection<WiimoteControlViewModel> _wiimoteTabs;
+        private ObservableCollection<WiimoteControlViewModel> _wiimoteTabs;
 
-		public ObservableCollection<WiimoteControlViewModel> WiimoteTabs
-		{
-			get { return _wiimoteTabs; }
-			private set
-			{
-				_wiimoteTabs = value;
-				OnPropertyChanged();
-			}
-		}
-
-		private WiimoteControlViewModel _selectedWiimoteTab;
-
-		public WiimoteControlViewModel SelectedWiimoteTab
-		{
-			get { return _selectedWiimoteTab; }
-			set { _selectedWiimoteTab = value;
-				OnPropertyChanged();
-			}
-		}
-
-
-		public WiimoteCollection WiimoteCollection { get; private set; }
-
-		/// <summary>
-		/// Installed camera
-		/// </summary>
-		public ICollection<FilterInfo> Devices
-		{
-			get { return _devices; }
-			private set
-			{
-				_devices = (List<FilterInfo>)value;
-				OnPropertyChanged();
-			}
-		}
-
-		/// <summary>
-		/// Selected camera
-		/// </summary>
-		public FilterInfo SelectedDevice
-		{
-			get { return _selectedDevice; }
-			set
-			{
-				_selectedDevice = value;
-				OnPropertyChanged();
-			}
-		}
-
-		/// <summary>
-		/// Current Image
-		/// </summary>
-		public BitmapImage CurrentImage
-		{
-			get { return _currentImage; }
-			set
-			{
-				_currentImage = value;
-				OnPropertyChanged();
-			}
-		}
-
-		public BitmapImage VideoButtonImage
-		{
-			get { return _videoButtonImage; }
-			set
-			{
-				_videoButtonImage = value;
-				OnPropertyChanged();
-			}
-		}
-
-		private ObservableCollection<ARObjectViewModel> _augmentedRealityObjects;
-
-		public ObservableCollection<ARObjectViewModel> AugmentedRealityObjects
-		{
-			get { return _augmentedRealityObjects; }
-			private set
-			{
-				_augmentedRealityObjects = value;
-				OnPropertyChanged();
-			}
-		}
-
-
-		public ICommand StartStopSendingToAzureCommand
-		{
-			get { return _startStopSendingToAzureCommand ?? (_startStopSendingToAzureCommand = new RelayCommand(StartStopSendingToAzure)); }
-		}
-
-		public ICommand StartStopVideoFeedCommand
-		{
-			get { return _startStopVideoFeedCommand ?? (_startStopVideoFeedCommand = new RelayCommand(StartStopVideoFeed)); }
-		}
-
-		private void StartStopVideoFeed()
-		{
-			if (SelectedDevice == null)
-				return;
-
-			if (_videoSource != null && _videoSource.IsRunning)
-			{
-				Stop();
-			}
-			else
-			{
-				Start();
-			}
-		}
-
-		private void Start()
-		{
-			CloseVideoSource();
-			VideoButtonImage = Application.Current.Resources["StopImage"] as BitmapImage;
-
-			_videoSource = new VideoCaptureDevice(SelectedDevice.MonikerString);
-			_videoSource.NewFrame += OnNewFrameReceived;
-			_videoSource.Start();
-		}
-
-		private void OnNewFrameReceived(object sender, NewFrameEventArgs eventArgs)
-		{
-			var img = (Bitmap)eventArgs.Frame.Clone();
-
-			foreach (var obj in AugmentedRealityObjects)
-			{
-				if (obj.SpaceWidth != img.Width)
-					obj.SpaceWidth = img.Width;
-				if (obj.SpaceHeight != img.Height)
-					obj.SpaceHeight = img.Height;
-			}
-
-			_synchronizationContext.Post(o =>
-			{
-				CurrentImage = BitmapConverter.ToBitmapImage(img);
-			}, null);
-		}
-
-		private void Stop()
-		{
-			CloseVideoSource();
-			VideoButtonImage = Application.Current.Resources["StartImage"] as BitmapImage;
-			CurrentImage = null;
+        public ObservableCollection<WiimoteControlViewModel> WiimoteTabs
+        {
+            get { return _wiimoteTabs; }
+            private set
+            {
+                _wiimoteTabs = value;
+                OnPropertyChanged();
+            }
         }
 
-		private void CloseVideoSource()
-		{
-			if (_videoSource == null) return;
-			if (!_videoSource.IsRunning) return;
-			_videoSource.SignalToStop();
-			_synchronizationContext.Post(o =>
-			{
-				CurrentImage = null;
-			}, null);
-		}
+        private WiimoteControlViewModel _selectedWiimoteTab;
 
-		private void StartStopSendingToAzure()
-		{
-			
-		}
+        public WiimoteControlViewModel SelectedWiimoteTab
+        {
+            get { return _selectedWiimoteTab; }
+            set
+            {
+                _selectedWiimoteTab = value;
+                OnPropertyChanged();
+            }
+        }
 
-		public MainWindowViewModel()
-		{
-			_synchronizationContext = SynchronizationContext.Current;
-			AugmentedRealityObjects = new ObservableCollection<ARObjectViewModel>();
-			VideoButtonImage = Application.Current.Resources["StartImage"] as BitmapImage;
 
-			InitializeWebCamList();
-			InitializeWiimoteList();
+        public WiimoteCollection WiimoteCollection { get; private set; }
 
-		}
-		
+        /// <summary>
+        /// Installed camera
+        /// </summary>
+        public ICollection<FilterInfo> Devices
+        {
+            get { return _devices; }
+            private set
+            {
+                _devices = (List<FilterInfo>)value;
+                OnPropertyChanged();
+            }
+        }
 
-		private void InitializeWebCamList()
-		{
-			SelectedDevice = null;
-			Devices = null;
+        /// <summary>
+        /// Selected camera
+        /// </summary>
+        public FilterInfo SelectedDevice
+        {
+            get { return _selectedDevice; }
+            set
+            {
+                _selectedDevice = value;
+                OnPropertyChanged();
+            }
+        }
 
-			_videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
-			if (_videoDevices.Count == 0)
-				return;
+        /// <summary>
+        /// Current Image
+        /// </summary>
+        public BitmapImage CurrentImage
+        {
+            get { return _currentImage; }
+            set
+            {
+                _currentImage = value;
+                OnPropertyChanged();
+            }
+        }
 
-			Devices = _videoDevices.Cast<FilterInfo>().ToList();
-			SelectedDevice = Devices.First();
-			//StartStopVideoFeed();
-		}
+        public BitmapImage VideoButtonImage
+        {
+            get { return _videoButtonImage; }
+            set
+            {
+                _videoButtonImage = value;
+                OnPropertyChanged();
+            }
+        }
 
+        private ObservableCollection<ARObjectViewModel> _augmentedRealityObjects;
+
+        public ObservableCollection<ARObjectViewModel> AugmentedRealityObjects
+        {
+            get { return _augmentedRealityObjects; }
+            private set
+            {
+                _augmentedRealityObjects = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public ICommand StartStopSendingToAzureCommand
+        {
+            get { return _startStopSendingToAzureCommand ?? (_startStopSendingToAzureCommand = new RelayCommand(StartStopSendingToAzure)); }
+        }
+
+        public ICommand StartStopVideoFeedCommand
+        {
+            get { return _startStopVideoFeedCommand ?? (_startStopVideoFeedCommand = new RelayCommand(StartStopVideoFeed)); }
+        }
+
+        private void StartStopVideoFeed()
+        {
+            if (SelectedDevice == null)
+                return;
+
+            if (_videoSource != null && _videoSource.IsRunning)
+            {
+                Stop();
+            }
+            else
+            {
+                Start();
+            }
+        }
+
+        private void Start()
+        {
+            CloseVideoSource();
+            VideoButtonImage = Application.Current.Resources["StopImage"] as BitmapImage;
+
+            _videoSource = new VideoCaptureDevice(SelectedDevice.MonikerString);
+            _videoSource.NewFrame += OnNewFrameReceived;
+            _videoSource.Start();
+        }
+
+        private void OnNewFrameReceived(object sender, NewFrameEventArgs eventArgs)
+        {
+            var img = (Bitmap)eventArgs.Frame.Clone();
+
+            foreach (var obj in AugmentedRealityObjects)
+            {
+                if (obj.SpaceWidth != img.Width)
+                    obj.SpaceWidth = img.Width;
+                if (obj.SpaceHeight != img.Height)
+                    obj.SpaceHeight = img.Height;
+            }
+
+            _synchronizationContext.Post(o =>
+            {
+                CurrentImage = BitmapConverter.ToBitmapImage(img);
+            }, null);
+        }
+
+        private void Stop()
+        {
+            CloseVideoSource();
+            VideoButtonImage = Application.Current.Resources["StartImage"] as BitmapImage;
+            CurrentImage = null;
+        }
+
+        private void CloseVideoSource()
+        {
+            if (_videoSource == null) return;
+            if (!_videoSource.IsRunning) return;
+            _videoSource.SignalToStop();
+            _synchronizationContext.Post(o =>
+            {
+                CurrentImage = null;
+            }, null);
+        }
+
+        private void StartStopSendingToAzure()
+        {
+
+        }
+
+        public MainWindowViewModel()
+        {
+            _synchronizationContext = SynchronizationContext.Current;
+            AugmentedRealityObjects = new ObservableCollection<ARObjectViewModel>();
+            VideoButtonImage = Application.Current.Resources["StartImage"] as BitmapImage;
+
+            InitializeWebCamList();
+            InitializeWiimoteList();
+
+        }
+
+
+        private void InitializeWebCamList()
+        {
+            SelectedDevice = null;
+            Devices = null;
+
+            _videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (_videoDevices.Count == 0)
+                return;
+
+            Devices = _videoDevices.Cast<FilterInfo>().ToList();
+            SelectedDevice = Devices.First();
+            //StartStopVideoFeed();
+        }
+
+        /// <summary>
+        /// Determines the number of wiimotes and initializes them, if any. Throughs an exception if no devices.
+        /// </summary>
 		private void InitializeWiimoteList()
-		{
-			WiimoteTabs = null;
-			WiimoteCollection = new WiimoteCollection();
-			WiimoteTabs = new ObservableCollection<WiimoteControlViewModel>();
-			int index = 1;
+        {
+            WiimoteTabs = null;
+            WiimoteCollection = new WiimoteCollection();
+            WiimoteTabs = new ObservableCollection<WiimoteControlViewModel>();
+            int index = 1;
 
-			try
-			{
-				WiimoteCollection.FindAllWiimotes();
-			}
-			catch (WiimoteNotFoundException ex)
-			{
-				MessageBox.Show(ex.Message, Properties.Resources.WiimoteNotFound, MessageBoxButton.OK, MessageBoxImage.Error);
-			}
-			catch (WiimoteException ex)
-			{
-				MessageBox.Show(ex.Message, Properties.Resources.WiimoteError, MessageBoxButton.OK, MessageBoxImage.Error);
-			}
+            if (WiimoteCollection.Count == 0)
+            {
+                MessageBox.Show("DEBUG_VTH: Wiimote not found.", Properties.Resources.WiimoteNotFound, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                try
+                {
+                    WiimoteCollection.FindAllWiimotes();
+                }
+                catch (WiimoteNotFoundException ex)
+                {
+                    MessageBox.Show(ex.Message, Properties.Resources.WiimoteNotFound, MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                catch (WiimoteException ex)
+                {
+                    MessageBox.Show(ex.Message, Properties.Resources.WiimoteError, MessageBoxButton.OK, MessageBoxImage.Error);
+                }
 
-			foreach (Wiimote wm in WiimoteCollection)
-			{
-				var tab = new WiimoteControlViewModel(wm) { Header = "Wiimote " + index };
-                WiimoteTabs.Add(tab);
+                foreach (Wiimote wm in WiimoteCollection)
+                {
+                    var tab = new WiimoteControlViewModel(wm) { Header = "Wiimote " + index };
+                    WiimoteTabs.Add(tab);
 
-				wm.Connect();
-				wm.SetReportType(InputReport.IRExtensionAccel, IRSensitivity.Maximum, true);
-				wm.SetLEDs(index++);
+                    wm.Connect();
+                    wm.SetReportType(InputReport.IRExtensionAccel, IRSensitivity.Maximum, true);
+                    wm.SetLEDs(index++);
 
-				var saber = new LightSaber(wm);
+                    var saber = new LightSaber(wm);
 
-				AugmentedRealityObjects.Add(saber);
+                    AugmentedRealityObjects.Add(saber);
 
-				//wm.WiimoteChanged += Wm_WiimoteChanged;
-			}
-
-		}
-
-		private EventHub.EventHubSender Sender { get; set; }
-
-		private void Wm_WiimoteChanged(object sender, WiimoteChangedEventArgs e)
-		{
-			if (Sender == null)
-				Sender = new EventHub.EventHubSender(ConfigurationManager.AppSettings["EventHub.Namespace"]);
-
-			Sender.Send(e.WiimoteState, ((Wiimote)sender).ID.ToString());
+                    //wm.WiimoteChanged += Wm_WiimoteChanged;
+                }
+            }
         }
 
-		public void Dispose()
-		{
-			foreach (Wiimote wm in WiimoteCollection)
-				wm.Disconnect();
-			GC.SuppressFinalize(this);
-		}
-	}
+        private EventHub.EventHubSender Sender { get; set; }
+
+        private void Wm_WiimoteChanged(object sender, WiimoteChangedEventArgs e)
+        {
+            if (Sender == null)
+                Sender = new EventHub.EventHubSender(ConfigurationManager.AppSettings["EventHub.Namespace"]);
+
+            Sender.Send(e.WiimoteState, ((Wiimote)sender).ID.ToString());
+        }
+
+        public void Dispose()
+        {
+            foreach (Wiimote wm in WiimoteCollection)
+                wm.Disconnect();
+            GC.SuppressFinalize(this);
+        }
+    }
 }
