51f1e55febcd543660f8a3fa5f8ea3321d301486
Vincent Thavo Live Viz <vincent.thavonekham@viseo.com>
2015-04-15 03:34:23 +0200

    Add the 2935 (!) files to the Web project

---
+++ Viseo.WiiWars.Web/Compiler/Preprocess/RazorPreCompilation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+
+namespace Viseo.WiiWars.Web.Compiler.Preprocess
+{
+    public class RazorPreCompilation : RazorPreCompileModule
+    {
+        public RazorPreCompilation(IServiceProvider provider) : base(provider)
+        {
+        }
+    }
+}
